31dfba2709a518776cb42246b0e5ab19bb1a8942
E553ZW <radhachandan9027@gmail.com>
2019-11-07 15:25:19 +0530

    Commit w.r.t loginView design complete

--- CatchUp/Components/LoginView.js
+++ CatchUp/Components/LoginView.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, TextInput, StyleSheet, Alert, Dimensions, TouchableOpacity, Image } from 'react-native';
-import { Button } from 'react-native-elements';
-
+import { View, TextInput, StyleSheet, Alert, Dimensions, Image, SectionStyle } from 'react-native';
+import { Button, Text } from 'react-native-elements';
 const LoginView = (props) => {
     var userName = "";
     var userPassword = "";
@@ -21,8 +20,8 @@ const LoginView = (props) => {
     };
 
     const signUpActionHandler = () => {
+        'Signup Tapped'
         Alert.alert(
-            'Signup Tapped'
         )
     };
 
@@ -36,12 +35,20 @@ const LoginView = (props) => {
         <View style={{ backgroundColor: 'transparent', flex: 1 }}>
             <Image source={require('../Assets/CatchUpBackground.png')} style={styles.backgroundImage} />
             <View style={styles.loginView}>
-                <TextInput placeholder='Email' style={styles.userInputView} onChangeText={usernameInputhandler} value={userPassword}></TextInput>
-                <TextInput placeholder='Password' secureTextEntry = 'true' style={styles.passwordInputView} onChangeText={userPasswordInputhandler} value={userName} ></TextInput>
-                <Button buttonStyle={styles.loginButton} title='Login' onPress={loginActionHandler} />
+                <Image source={require('../Assets/CatchUpLogo.png')} style={styles.appLogo} />
+                <Text style={styles.appLogoTitle} >Find your Heart</Text>
+                <View style={styles.SectionStyle}>
+                    <Image source={require('../Assets/email.png')} style={styles.ImageStyle} />
+                    <TextInput style={styles.textInput} placeholder="Email" underlineColorAndroid="transparent" />
+                </View>
+                <View style={styles.SectionStyle}>
+                    <Image source={require('../Assets/password.png')} style={styles.ImageStyle} />
+                    <TextInput style={styles.textInput} placeholder="Password" underlineColorAndroid="transparent" />
+                </View>
+                <Button buttonStyle= {styles.loginButton}  titleStyle={styles.buttonTitle} title='Login' onPress={loginActionHandler} />
                 <View style={styles.bottomView}>
-                    <Button buttonStyle={styles.bottomButton} title='Signup' onPress={signUpActionHandler} />
-                    <Button buttonStyle={styles.bottomButton} title='Forgot Password' onPress={forgotPasswordActionHandler} />
+                    <Button buttonStyle={styles.bottomButton} titleStyle={styles.buttonTitle} title='Sign up'  onPress={signUpActionHandler} />
+                    <Button buttonStyle={styles.bottomButton} titleStyle={styles.buttonTitle} title='Forgot Password' onPress={forgotPasswordActionHandler} />
                 </View>
             </View>
         </View>
@@ -58,23 +65,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         bottom: 0
     },
-
-    userInputView: {
-        margin: 10,
-        marginBottom: 5,
-        padding: 10,
-        height: 44,
-        width: '80%',
-        borderRadius: 20,
-        backgroundColor: 'white'
-    },
-    passwordInputView: {
-        padding: 10,
-        height: 44,
-        width: '80%',
-        borderRadius: 20,
-        backgroundColor: 'white'
-    },
     loginButton: {
         backgroundColor: '#a34277',
         margin: 15,
@@ -85,13 +75,29 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 1, height: 1, },
         shadowColor: 'black',
         shadowOpacity: 1,
+        fontFamily: 'KohinoorTelugu-Regular',
+        fontSize: 14
     },
 
-    bottomButton: { backgroundColor: 'transparent', width: Dimensions.get('window').width / 2, height: 60 },
+    textInput:{
+     flex: 1,
+     fontSize: 13,
+     fontFamily: 'KohinoorTelugu-Medium'
+    },
+
+    buttonTitle: {
+        color: "white",
+        fontSize: 14,
+        fontFamily: 'KohinoorTelugu-Medium'
+    },
+    bottomButton: {
+        backgroundColor: 'transparent', width: Dimensions.get('window').width / 2, height: 60, fontFamily: 'KohinoorTelugu-Medium',
+        fontSize: 16
+    },
     bottomView: {
         width: '100%',
         height: 60,
-        backgroundColor: '#a34277',
+        backgroundColor: '#a64276',
         justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
@@ -101,6 +107,34 @@ const styles = StyleSheet.create({
     backgroundImage: {
         flex: 1,
         resizeMode: 'cover'
+    },
+
+    appLogo: {
+        bottom: 150,
+        width: 100,
+        height: 100,
+        borderRadius: 10
+    },
+
+    appLogoTitle: { color: 'white', fontFamily: 'KohinoorTelugu-Medium', fontSize: 13, textAlign: 'center', bottom: 147 },
+    SectionStyle: {
+        width: '80%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height: 45,
+        borderRadius: 20,
+        margin: 2,
+        padding: 10
+    },
+    ImageStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+        alignItems: 'center',
     }
 });
 
